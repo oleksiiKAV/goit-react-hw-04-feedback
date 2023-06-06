938a3f60aff8d3af0411ed215a71dee0f3aa2d09
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-06-06 12:30:22 +0000

    Deploying to gh-pages from @ oleksiiKAV/goit-react-hw-04-feedback@464c5ae1ae31e034fc7e13fc05edab8b4e4e3825 🚀

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Statistics } from '../components/Statistics/Statistics';
-export { default as FeedbackOptions } from '../components/FeedbackOptions/FeedbackOptions';
-export {default as Section} from '../components/Section/Section'
